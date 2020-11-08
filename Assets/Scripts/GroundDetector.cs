9ac1af7695ce67d93f2c68ed9ce0563de1780779
AbbaVaR <asr77734@gmail.com>
2020-11-08 16:29:48 +0500

    Initial commit

---
+++ Assets/Scripts/GroundDetector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetector : MonoBehaviour {
+
+	public MainChapter player;
+	public LayerMask groundLayers;
+	private void OnTriggerEnter2D(Collider2D other)
+    {
+		if (1 << other.gameObject.layer == groundLayers)
+        {
+			player.isGrounded = true;
+        }
+
+    }
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (1 << other.gameObject.layer == groundLayers.value)
+		{
+			player.isGrounded = false;
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+	public GameObject player;
+	void Update()
+	{
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y+1.7f, -5f);
+	}
+}
