d70de15275ff8adb6d94a280d06933dd7002166a
AbbaVaR <asr77734@gmail.com>
2020-11-18 13:22:49 +0500

    Удалён лишний скрипт

--- Assets/Scripts/AboutPlayer.cs
+++ Assets/Scripts/AboutPlayer.cs
@@ -7,14 +7,23 @@ public class AboutPlayer : MonoBehaviour {
     public float MaxStam { get; set; } = 100f;
     public float CurStam { get; set; }
     public float PlayerD { get; set; } = 30f;
+    public float Speed { get; } = 5f;
+    public float JumpForce { get; } = 7000f;
+    public bool IsFacingRight { get; set; } = true;
+    public bool IsGrounded { get; set; } = false;
+    public float GroundRadius { get; } = 0.1f;
+    public float AttackRadius { get;} = 0.35f;
+    public bool Blocking { get; set; } = false;
 
     public Animator anim;
-    //public Image hpBar;
-    //public float FillHpBar { get; set; } = 1f;
+    public Rigidbody2D rb;    
+    public Transform attackCheck;    
+    public LayerMask whatIsGround;    
+    public Transform groundCheck;
 
     private void Start()
     {
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
     }
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,23 +11,18 @@ public class Player : AboutPlayer
      
     void FixedUpdate()
     {
+        ControlPlayer();
     }
 
     public void PlayerDamage(float dam)
     {
-        //if (!pM.Blocking) 
+        if (!Blocking) 
         CurHP -= dam; 
         if (CurHP <= 0)
         {
             Die(); 
         }
     }
-
-    /*private void HPbar()
-    {
-        FillHpBar =  CurHP/MaxHP;
-        hpBar.fillAmount = FillHpBar;
-    }*/
 
     void Die()
     {
@@ -38,5 +33,60 @@ public class Player : AboutPlayer
     {
         yield return new WaitForSeconds(timeToWait);
         Destroy(gameObject);
+    }
+
+    private void Flip()
+    {
+        IsFacingRight = !IsFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void ControlPlayer()
+    {
+
+        IsGrounded = Physics2D.OverlapCircle(groundCheck.position, GroundRadius, whatIsGround);
+        anim.SetBool("Grounded", IsGrounded);
+        //движение
+        float moveX = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(moveX));
+        if (!Blocking)
+            rb.MovePosition(rb.position + Vector2.right * moveX * Speed * Time.deltaTime);
+        if (moveX > 0 && !IsFacingRight && !Input.GetKey(KeyCode.Mouse1))
+            Flip();
+        else if (moveX < 0 && IsFacingRight && !Input.GetKey(KeyCode.Mouse1))
+            Flip();
+        //прыжок
+        if (IsGrounded && Input.GetKeyDown(KeyCode.Space) && !Blocking)
+        {
+            anim.SetBool("Grounded", false);
+            rb.AddForce(Vector2.up * JumpForce);
+        }
+        //атака
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetBool("Attacking", true);
+            Fight2D.Action(attackCheck.position, AttackRadius, 9, PlayerD, false);
+        }
+
+        else
+        {
+            anim.SetBool("Attacking", false);
+        }
+        //блок
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Blocking = true;
+            anim.Play("Player_block");
+
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            Blocking = false;
+            anim.Play("Player_idle");
+        }
+
     }
 }
